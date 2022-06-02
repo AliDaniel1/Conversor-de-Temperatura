b895e8997109f4b9b8cb522496a9c322e3c77bac
AliDaniel1 <alidanielh123@gmail.com>
2022-06-01 21:27:14 -0430

    Commit#3(Opciones y estilo)

--- script.js
+++ script.js
@@ -14,3 +14,31 @@ function FarenheitaCelsius(){
     let Respuesta = (T - 32) * (5/9);
     Resultado.innerHTML = `${Respuesta}°C`;
 }
+
+function CelsiusaKelvin(){
+    T = parseInt(Temperatura.value);
+
+    let Respuesta = T + 273.15;
+    Resultado.innerHTML =`${Respuesta}°K`;
+}
+
+function KelvinaCelsius(){
+    T = parseInt(Temperatura.value);
+
+    let Respuesta = T - 273.15;
+    Resultado.innerHTML =`${Respuesta}°C`;
+}
+
+function KelvinaFarenheit(){
+    T = parseInt(Temperatura.value);
+
+    let Respuesta = (T - 273.15) * 1.8 + 32;
+    Resultado.innerHTML =`${Respuesta}°F`;
+}
+
+function FarenheitaKelvin(){
+    T = parseInt(Temperatura.value);
+
+    let Respuesta = (T - 32) * 5/9 + 273.15;
+    Resultado.innerHTML =`${Respuesta}°K`;
+}
