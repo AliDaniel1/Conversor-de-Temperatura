370ed38df83c338d2c3bc4da5176dc0a93577b3a
AliDaniel1 <alidanielh123@gmail.com>
2022-06-04 16:59:34 -0430

    Commit#5(Correcion de decimales)

--- script.js
+++ script.js
@@ -4,41 +4,41 @@ var Resultado = document.getElementById("Resultado");
 CelsiusaFarenheit = ()=>{
     T = parseInt(Temperatura.value);
 
-    let Respuesta = (T * 1.8) + 32;
+    let Respuesta = Number(((T * 1.8) + 32).toFixed(2));
     Resultado.innerHTML = `${Respuesta}°F`;
 }
 
 FarenheitaCelsius = ()=>{
     T = parseInt(Temperatura.value);
 
-    let Respuesta = (T - 32) * (5/9);
+    let Respuesta = Number(((T - 32) * (5/9)).toFixed(2));
     Resultado.innerHTML = `${Respuesta}°C`;
 }
 
 CelsiusaKelvin = ()=>{
     T = parseInt(Temperatura.value);
 
-    let Respuesta = T + 273.15;
+    let Respuesta = Number((T + 273.15).toFixed(2));
     Resultado.innerHTML =`${Respuesta}°K`;
 }
 
 KelvinaCelsius = ()=>{
     T = parseInt(Temperatura.value);
 
-    let Respuesta = T - 273.15;
+    let Respuesta = Number((T - 273.15).toFixed(2));
     Resultado.innerHTML =`${Respuesta}°C`;
 }
 
 KelvinaFarenheit = ()=>{
     T = parseInt(Temperatura.value);
 
-    let Respuesta = (T - 273.15) * 1.8 + 32;
+    let Respuesta = Number((T - 273.15) * 1.8 + 32).toFixed(2);
     Resultado.innerHTML =`${Respuesta}°F`;
 }
 
 FarenheitaKelvin = ()=>{
     T = parseInt(Temperatura.value);
 
-    let Respuesta = (T - 32) * 5/9 + 273.15;
+    let Respuesta = Number(((T - 32) * 5/9 + 273.15).toFixed(2));
     Resultado.innerHTML =`${Respuesta}°K`;
 }
